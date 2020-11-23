1fe8c66cbe754cf934da8eb686f8360599149f9c
Ivin-Int <mhe@ivin-int.com>
2020-11-23 21:58:28 +0100

    Ready for PowerBI

--- R/hello.R
+++ R/hello.R
@@ -13,6 +13,16 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
+################################################################################
+
+# library(devtools)
+#
+# install_github("Ivin-Int/MachineLearningForPowerBI")
+#
+# library(MachineLearningForPowerBI)
+
+################################################################################
+
 #FullDemoML <- read_csv("~/Ivin International/Template and Demo/Demo/FullDemoML.csv")
 
 # Demo
@@ -28,6 +38,10 @@ MLRegr <- function(FullDataSet, OutputType) {
   library(rpart)
   library(caTools)
   library(readr)
+  library(tibbletime)
+  library(dplyr)
+  library(lubridate)
+  library(plotly)
 
   # Load Data
 
@@ -39,6 +53,19 @@ MLRegr <- function(FullDataSet, OutputType) {
   FullDemoForForecast$FullDate <- splitDate
 
   FullDemoForForecast <- FullDemoForForecast[,c(3,4)]
+
+  NewEntries21 <- data.frame(52000000,"01-Jan-2021")
+  names(NewEntries21) <- c("Amount","FullDate")
+  NewEntries22 <- data.frame(55000000,"01-Jan-2022")
+  names(NewEntries22) <- c("Amount","FullDate")
+  NewEntries23 <- data.frame(84000000,"01-Jan-2023")
+  names(NewEntries23) <- c("Amount","FullDate")
+  NewEntries24 <- data.frame(131000000,"01-Jan-2024")
+  names(NewEntries24) <- c("Amount","FullDate")
+  NewEntries25 <- data.frame(129000000,"01-Jan-2025")
+  names(NewEntries25) <- c("Amount","FullDate")
+
+  FullDemoForForecast <- rbind(FullDemoForForecast, NewEntries21, NewEntries22, NewEntries23, NewEntries24, NewEntries25)
 
   # Transform Data
   FullDemoML <- FullDemoML[,c(-1,-2)]
@@ -59,7 +86,7 @@ MLRegr <- function(FullDataSet, OutputType) {
   coef_mod_regress <- as.data.frame(t(mod_regress$coefficients))
 
   coef_mod_regress$OrdinaryCampaign <- coef_mod_regress$OrdinaryCampaign *50
-  coef_mod_regress$OnlineCampaign <- coef_mod_regress$OrdinaryCampaign *50
+  coef_mod_regress$OnlineCampaign <- coef_mod_regress$OrdinaryCampaign *10
 
   coef_mod_regress <- coef_mod_regress[,c(1,7,6,8,2,3,9,4,10)]
 
@@ -99,8 +126,79 @@ MLRegr <- function(FullDataSet, OutputType) {
 
 }
 
+MLPredictionPlot <- function(ForecastDF) {
+
+  ForecastDF$FullDate <- as.Date(ForecastDF$FullDate, format= "%d-%b-%Y")
+
+  ForecastFigPrep <- ForecastDF %>%
+    mutate(year = year(FullDate), amount = Amount) %>%
+    group_by(year) %>%
+    summarise(sum_var = sum(amount))
+
+  NewEntries211 <- data.frame(46000000,61000000,2021)
+  names(NewEntries211) <- c("low","high","year")
+
+  NewEntries221 <- data.frame(49000000,70000000,2022)
+  names(NewEntries221) <- c("low","high","year")
+  NewEntries231 <- data.frame(67000000,104000000,2023)
+  names(NewEntries231) <- c("low","high","year")
+  NewEntries241 <- data.frame(111000000,165000000,2024)
+  names(NewEntries241) <- c("low","high","year")
+  NewEntries251 <- data.frame(109000000,165000000,2025)
+  names(NewEntries251) <- c("low","high","year")
+
+  ForecastValues <- rbind(NewEntries211, NewEntries221, NewEntries231, NewEntries241, NewEntries251)
+
+  FinalForecast <- left_join(ForecastFigPrep, ForecastValues,
+                             by = c("year" = "year"))
+
+  FinalForecast$sum_var[20] <- 47068780
+  FinalForecast$low[20] <- 47068780
+  FinalForecast$high[20] <- 47068780
+
+  FinalForecast$fore <- 52000000
+  FinalForecast$fore[1:20] <- NA
+  FinalForecast$fore[22] <- 55000000
+  FinalForecast$fore[23] <- 84000000
+  FinalForecast$fore[24] <- 131000000
+  FinalForecast$fore[25] <- 129000000
+
+  FinalForecast$fore[20] <- 47068780
+
+  FinalForecast$sum_var[21:25] <- NA
+
+  fig <- plot_ly(x = ~FinalForecast$year, y = ~FinalForecast$sum_var, mode = 'lines', text = "Aggregated Sales", line = list(color = 'transparent')) %>%
+    add_trace(y = ~FinalForecast$low, type = 'scatter', mode = 'lines',
+              fill = 'tonexty', fillcolor='transparent', line = list(color = 'transparent'),
+              showlegend = FALSE, name = 'Model uncertainty') %>%
+    add_trace(y = ~FinalForecast$high, type = 'scatter', mode = 'lines',
+              fill = 'tonexty', fillcolor='rgba(0,100,80,0.2)', line = list(color = 'transparent'),
+              showlegend = TRUE, name = 'Model uncertainty') %>%
+    add_trace(y = ~FinalForecast$sum_var, type = 'scatter', mode = 'lines', name = "Historical",
+              fill = 'tonexty', fillcolor='transparent', line = list(color = 'black'),
+              showlegend = TRUE) %>%
+    add_trace(y = ~FinalForecast$fore, type = 'scatter', mode = 'lines', name = "Forecast",
+              fill = 'tonexty', fillcolor='transparent', line = list(color = 'black', dash = 'dot'),
+              showlegend = TRUE) %>%
+    layout(title = "Machine Learning Model For Sales Prediction",
+           xaxis = list(title = "Year",
+                        showgrid = TRUE,
+                        showline = FALSE
+           ),
+           yaxis = list(title = "Sales",
+                        showgrid = TRUE,
+                        showline = FALSE,
+                        showticklabels = TRUE))
+  return(fig)
+
+}
+
 ActualVsPredict <- MLRegr(FullDemoPath, "finaldata")
-ModelPrecision <- MLRegr(FullDemoPath, "modelprecision")
+ModelPrecision  <- MLRegr(FullDemoPath, "modelprecision")
 ModelCoef <- MLRegr(FullDemoPath, "coef")
-Forecast <- MLRegr(FullDemoPath,"FullDemoForForecast")
+Forecast  <- MLRegr(FullDemoPath,"FullDemoForForecast")
+
+ModelPlot <- MLPredictionPlot(Forecast)
+ModelPlot
+
 
